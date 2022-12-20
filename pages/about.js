2ee5ec714ef4c1d856f1cef108c5e0c0d446cbd2
bifinbabu <bifinbabu088@gmail.com>
2022-12-20 11:23:12 +0530

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const About = () => {
+    return ( 
+        <>
+        <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+        </Head>
+        <div>
+            <h1>Aboyut</h1>
+            <p>Loren iijfsgjh kjgdsh j jsdgh jhdsgf  hjfg hgfj  dhgsfj ds</p>
+            <p>Loren iijfsgjh kjgdsh j jsdgh jhdsgf  hjfg hgfj  dhgsfj ds</p>
+            <p>Loren iijfsgjh kjgdsh j jsdgh jhdsgf  hjfg hgfj  dhgsfj ds</p>
+        </div>
+        </>
+     );
+}
+ 
+export default About;
